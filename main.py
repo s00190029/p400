359b5326688f5000b545aff9f196db2c75ae7cc4
s00190029 <mrdaylight@protonmail.com>
2023-04-10 19:54:05 +0100

    fixed import order

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import platform
 import asyncio
 from enum import Enum
-import wgui
-import lgui4
 
 def get_current_os():
     return platform.system()
@@ -12,8 +10,10 @@ cos_first_letter =  current_os[0].lower()
 
 match cos_first_letter:
     case "w":
+        import wgui
         asyncio.run(wgui.wgui())
     case "l":
+        import lgui4
         asyncio.run(lgui4.lgui())
     case 'm':
         print("Mac not yet supported")
